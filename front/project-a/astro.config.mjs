ab6d803b8e034c4a64935d158c0e9f3226abca6e
Esteban Velez <tebanvelez91@gmail.com>
2026-03-11 00:07:40 -0500

    cambios en front

--- front/project-a/astro.config.mjs
+++ front/project-a/astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte"; // Importa la integración de Svelte
+import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
+import bun from '@astrojs/bun'; // <--- Añade esto
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],  // Agrega la integración de Svelte
-  output: 'server' 
+  output: 'server',
+  adapter: bun(), // <--- Y esto
+  integrations: [tailwind(), svelte()],
 });
