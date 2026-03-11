c4b8bbfedc77e404e4c5b909d293dc4f8f5a6405
Esteban Velez <tebanvelez91@gmail.com>
2026-03-10 21:19:08 -0500

    Se cambian constantes por variables en el archivo de propiedades

--- ms-database-analysis/infrastructure/entry-points/api-rest/src/main/java/co/com/savia/api/security/JwtTokenProvider.java
+++ ms-database-analysis/infrastructure/entry-points/api-rest/src/main/java/co/com/savia/api/security/JwtTokenProvider.java
@@ -1,27 +1,41 @@
 package co.com.savia.api.security;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import java.util.Date;
+import java.util.Objects;
 
 @Component
 public class JwtTokenProvider {
 
-    private final SecretKey secretKey = Keys.hmacShaKeyFor("4oClZf4F3KnF2s5p9L+FqgP+zA0KQkPyQ9l0u4Oel3U="
-            .getBytes(StandardCharsets.UTF_8));
+    private final Environment environment;
+    private final SecretKey secretKey;
+    // Inyectamos el Environment en el constructor
+    public JwtTokenProvider(Environment environment) {
+        this.environment = environment;
+        // Obtenemos el secreto
+        String secret = environment.getProperty("jwt.secret");
+
+        // Validamos que no sea nulo (opcional pero recomendado)
+        Objects.requireNonNull(secret, "La propiedad 'jwt.secret' no puede ser nula");
+
+        // Inicializamos la llave. Al estar en el constructor, ya podemos usar 'final'
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+
 
     public String generateToken(String username, String role) {
-        // 1 día en milisegundos
-        long expirationTime = 432000000;
+        long expirationTime = Long.parseLong(Objects.requireNonNull(environment.getProperty("jwt.expiration")));
         return Jwts.builder()
                 .setSubject(username)
-                .claim("role", role) // Añade el rol al token
+                .claim("role", role)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
                 .signWith(secretKey, SignatureAlgorithm.HS256)
