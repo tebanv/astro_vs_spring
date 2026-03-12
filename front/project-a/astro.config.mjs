64c1a222cfc73407d2ac9111fa874831453f3f17
Echava <danielechavarria9@hotmail.com>
2026-03-12 19:55:35 +0000

    config: agregar adaptador de node para ssr

--- front/project-a/astro.config.mjs
+++ front/project-a/astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte"; // Importa la integración de Svelte
+import node from '@astrojs/node'; //Adaptador
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],  // Agrega la integración de Svelte
-  output: 'server' 
+  output: 'server', 
+  adapter: node({
+    mode: 'standalone' // Clave para que Bun levante el servidor por su cuenta
+  }),
 });
